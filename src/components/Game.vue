30e274a8bf4ba937f88655925d4e72c021f39a3c
Artem <lantan555@yandex.ru>
2020-07-07 20:56:23 +0300

    added firebase

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,11 +1,21 @@
 <template>
-  <v-container>
-    <v-row class="text-center" justify="center">
+  <v-container class="fill-height" >
+    <v-row class="text-center" justify="center" >
       <v-col cols="8">
-        <h1>Тест по JavaScript</h1>
-        <v-card>
+        <div v-if="question === null">
+          <v-row>
+            <v-col cols="4">
+              <v-btn block outlined class="" color="primary"  @click="startTest()">
+                1
+              </v-btn>
+            </v-col>
+          </v-row>
+          <h1>Выберите вариант теста:</h1>
+
+        </div>
+        <v-card v-else>
           <v-card-title>
-            test
+            JavaScript. Уровень 1
           </v-card-title>
           <v-card-subtitle>
 <!--            <div-->
@@ -17,97 +27,125 @@
 <!--            </div>-->
           </v-card-subtitle>
           <v-card-text>
-            <div
-                    v-for="(item, i) in 2"
-                    :key="i"
-                    :step="i+1"
-            >
-              <Timer ref="timer" @time-out="checkAnswer(selected, item)"/>
-              <h2>{{ i + 1 }}.
-                <span>{{ item.header }}</span>
+            <div >
+              <Timer ref="timer" @time-out="checkAnswer(selected, question)"/>
+              <h2>{{ question.id }}.
+                <span>{{ question.header }}</span>
               </h2>
               <v-card
-                      v-if="item.body"
+                      v-if="question.body"
                       class="mb-4 mt-4 pa-2 body-desc"
                       color="grey lighten-2"
               >
-                {{ item.body }}
+                {{ question.body }}
               </v-card>
               <v-radio-group v-model="selected">
-                <div v-for="(n, i) in item.answers" :key="`${i}-radio`">
+                <div v-for="(n, i) in question.answers" :key="`${i}-radio`">
                   <v-radio  class="radio mt-0 success--text" color="primary"  :disabled="disabled" :value="n.id" >
                     <template v-slot:label>
                       <span :class="{ 'success--text' : n.tips && showResult }">{{n.text}}</span>
                     </template>
                   </v-radio>
-                  <span class="tip" v-if="n.tips && showResult">
+                  <span class="tip success lighten-1 white--text" v-if="n.tips && showResult">
                       {{ n.tips }}
-                    </span>
+                  </span>
                 </div>
 
 
               </v-radio-group>
             </div>
           </v-card-text>
-
           <v-card-actions>
             <v-btn
-                    class="white--text  ma-2"
-                    :color="btn ? 'green' : 'primary'"
-                    @click="answer(item)"
+                    class=" ma-2"
+                    color="primary"
+                    @click="answer(question)"
                     :disabled="disabled"
-
             >
-              {{ btn ? 'Завершить' : 'Ответить' }}
+              Ответить
             </v-btn>
             <v-btn color="primary ma-2" :disabled="!disabled">
               Далее
             </v-btn>
           </v-card-actions>
         </v-card>
+
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-  import axios from 'axios'
+  // const db = require('../static/db.json')
+  import { db } from '../main'
+  // const answers = require('../static/db_ans.json')
 
+
+  class Test {
+    // constructor(props) {
+    //   this.id = props.id
+    //   this.name = props.name
+    // }
+
+    getLangAll(){
+
+      return db.collection('tests')
+              .get()
+              .then(querySnapshot => {
+                const document = querySnapshot.docs.map(doc => doc.data())
+                console.log(document)
+              })
+
+    }
+    getLang(id){
+
+        db.collection('tests')
+              .doc(id)
+              .get()
+              .then(snapshot => {
+                return  snapshot.data()
+              })
+
+    }
+
+  }
+
+  const test = new Test()
   export default {
     name: 'Game',
     components: {
       Timer: () => import('../components/Timer')
     },
+
     data: () => ({
-      e1: 1,
+      lang: [],
+      test: null,
+      el: 0,
       selected: null,
       value: 100,
       disabled: false,
       showResult: false,
-      questions: []
+      question: null,
+      answers: []
     }),
+
     mounted() {
-      axios
-        .get(`./src/static/db.json`)
-        .then(response => (console.log(response.data)))
-        .catch((e) => console.log(e))
+      test.getLangAll()
+      console.log('mounted', test.getLangAll())
+      test.getLang('wSquTpcqZkTbdQKryKYR' )
     },
     watch: {
 
     },
     computed: {
 
-      qnty() {
-        return this.questions.length
-      },
-      btn(){
-        return this.questions.length == this.e1
-      },
     },
 
     methods: {
-
-
+      startTest(value){
+        console.log(this.test)
+        this.test = db[value]
+      },
       timerOff(){
         this.disabled = true
         this.showResult = true
@@ -164,6 +202,8 @@
     border: 1px solid #000;
     font-size: 12px;
   }
+
+
 
 
 </style>
